670dc55962284e1d50af89624b19acf8f38d59e5
aryan <100193676+Aryan2807@users.noreply.github.com>
2024-07-23 10:20:42 +0530

    after score in page show done

--- logic.js
+++ logic.js
@@ -1,33 +1,44 @@
 let rock= document.getElementById("rock");
 let paper= document.getElementById("paper");
 let scissors= document.getElementById("scissors");
+let playerscore=document.getElementById("plscr");
+let aiscore=document.getElementById("aiscr");
 let a="";
 let b="";
+let player=0;
+let ai=0;
 let rockk=() =>{
     a="rock";
     AII();
     if (b==="rock") {
         alert('tie')
+
     }
     else if (b==="scissors"){
         alert("you won");
+        player+=1;
     }
     else if (b==="paper"){
         alert("you lost");
+        ai+=1;
     }
+    updatescores();
 };
 let paperr=() =>{
     a="paper";
     AII();
     if (b==="rock") {
         alert('you won')
+        player+=1;
     }
     else if (b==="scissors"){
         alert("you lost");
+        ai+=1;
     }
     else if (b==="paper"){
         alert("tie");
     }
+    updatescores();
 
 };
 let scissorss=()=>{
@@ -35,13 +46,16 @@ let scissorss=()=>{
     AII();
     if (b==="rock") {
         alert('you lost')
+        ai+=1;
     }
     else if (b==="scissors"){
         alert("tie");
     }
     else if (b==="paper"){
         alert("you won");
+        player+=1;
     }
+    updatescores();
 };
 let AII=()=>{
     c=Math.random();
@@ -55,6 +69,12 @@ let AII=()=>{
         b="scissors"
     }
 };
+
+let updatescores=()=>{
+    playerscore.innerText=`${player}`;
+    aiscore.innerText=`${ai}`;
+};
+
 rock.addEventListener("click",rockk);
 scissors.addEventListener("click",scissorss);
 paper.addEventListener("click",paperr);
