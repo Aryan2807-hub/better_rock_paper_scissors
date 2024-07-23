60c51be8dbdf17344a59e49345f86fff45ac774d
aryan <100193676+Aryan2807@users.noreply.github.com>
2024-07-23 15:44:52 +0530

    prefinal

--- logic.js
+++ logic.js
@@ -4,12 +4,24 @@ let scissors= document.getElementById("scissors");
 let playerscore=document.getElementById("plscr");
 let aiscore=document.getElementById("aiscr");
 let reset=document.getElementById("reset");
+let you = document.getElementById("you").querySelector("img");
+let comp = document.getElementById("comp").querySelector("img");
+let animation=document.getElementsByClassName("emoji");
+let nxtround=document.getElementById("next round");
+
+
+
 let a="";
 let b="";
 let player=0;
 let ai=0;
 playerscore.innerText=0;
 aiscore.innerText=0;
+const choices = {
+    "rock": "/emoji/rock-emoji.png",
+    "paper": "/emoji/paper-emoji.png",
+    "scissors": "/emoji/scissors-emoji.png"
+};
 
 
 
@@ -24,52 +36,76 @@ let updatescores=()=>{
 let rockk=() =>{
     a="rock";
     AII();
+    animate();
+
     if (b==="rock") {
-        alert('tie')
+
 
     }
     else if (b==="scissors"){
-        alert("you won");
+        
         player+=1;
+        
     }
     else if (b==="paper"){
-        alert("you lost");
+    
         ai+=1;
+       
     }
-    updatescores();
+    whowon();
+    
 };
 let paperr=() =>{
     a="paper";
     AII();
+    animate();
+
+    
+    
     if (b==="rock") {
-        alert('you won')
+       
         player+=1;
+        
+        
     }
     else if (b==="scissors"){
-        alert("you lost");
+       
         ai+=1;
+        
     }
     else if (b==="paper"){
-        alert("tie");
+     
+        
     }
-    updatescores();
+    whowon();
+    
+    
 
 };
 let scissorss=()=>{
     a="scissors";
     AII();
+    animate();
+
+    
+
     if (b==="rock") {
-        alert('you lost')
+       
         ai+=1;
+        
     }
     else if (b==="scissors"){
-        alert("tie");
+     
+       
     }
     else if (b==="paper"){
-        alert("you won");
+
         player+=1;
+       
     }
-    updatescores();
+    whowon();
+    
+    
 };
 let AII=()=>{
     c=Math.random();
@@ -84,7 +120,17 @@ let AII=()=>{
     }
 };
 
-
+let whowon=()=>{
+    if (player+=1){
+        console.log("player won")
+    }
+    else if (ai+=1){
+        console.log("computer won")
+    }
+    else{
+        console.log("tie")
+    }
+};
 
 let addreset=()=>{
     player=0;
@@ -92,6 +138,27 @@ let addreset=()=>{
     updatescores();
 };
 
+let nextround=()=>{ 
+    you.classList.remove("rotate-clockwise");
+    comp.classList.remove("rotate-anticlockwise");
+    updatescores();
+
+    resetImages();
+}
+let resetImages = () => {
+    you.src = choices["rock"];
+    comp.src = choices["rock"];
+};
+let animate = () => {
+    you.classList.add("rotate-clockwise");
+    comp.classList.add("rotate-anticlockwise");
+    setTimeout(() => {
+        you.src = choices[a];
+        comp.src = choices[b];
+       
+    }, 720);
+};
+nxtround.addEventListener("click",nextround);
 
 reset.addEventListener("click",addreset);
 rock.addEventListener("click",rockk);
